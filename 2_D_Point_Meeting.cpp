b21e330b28247e3bac448c909b2073a75d257d7c
Karan <karanbpawar@gmail.com>
2021-09-11 16:55:00 +0530

    k

--- 2_D_Point_Meeting.cpp
+++ 2_D_Point_Meeting.cpp
@@ -11,7 +11,7 @@ using namespace std;
 const ll inf=1e18+7;
 const pair<ll,ll> invalid={inf,inf};
 pair<ll,ll> intersect(pair<ll,ll> P1,pair<ll,ll> P2,pair<ll,ll> par1,pair<ll,ll> par2) {
-    if(par1==par2) return invalid;
+    // if(par1==par2) return invalid;
     if(P1.first==0) {
         if(P2.first==1) {
             return {P1.second-P2.second,P1.second};
@@ -63,6 +63,20 @@ pair<ll,ll> intersect(pair<ll,ll> P1,pair<ll,ll> P2,pair<ll,ll> par1,pair<ll,ll>
         }
     }
 }
+bool online(pair<ll,ll> a,pair<ll,ll> b) {
+    if(a.second==b.second) return true;
+    else if(a.first==b.first) return true;
+    else if(b.second-a.second==b.first-a.first) return true;
+    else if((b.second-a.second==a.first-b.first)) return true;
+    return false;
+}
+bool linept(pair<ll,ll> a, pair<ll,ll> b) {
+    if(a.first==inf) {
+        b.second=0;
+        a.first=1;
+    }
+    return (b.second==((a.first*b.first)+a.second));
+}
 void solve() {
     ll n;
     cin>>n;
@@ -98,15 +112,8 @@ void solve() {
     ll maxiy=ya[0].first;
 
     ll total=2*n;
-    ll ans=2*n;
-    for(ll i=0;i<xa.size() and xa[i].first==maxix ;i++) {
-        for(ll j=0;j<ya.size() and ya[i].first==maxiy ;j++) {
-            ll temp=0;
-            if(s.find({xa[i].second,ya[i].second})!=s.end()) temp++;
-            ans=min(ans,total-maxix-maxiy-temp);
-        }
-    }
-    vector<pair<pair<ll,ll>,pair<int,int>>> lines;
+
+    vector<pair<pair<ll,ll>,pair<ll,ll>>> lines;
     for(ll i=0;i<n;i++) {
         lines.push_back({{0,arr[i].second},arr[i]});
         lines.push_back({{1,arr[i].second-arr[i].first},arr[i]});
@@ -114,35 +121,86 @@ void solve() {
         lines.push_back({{inf,-arr[i].first},arr[i]});
     }
     ll ans2=2*n;
-    map<pair<double,double>,ll> M;
+    map<pair<ll,ll>,set<pair<ll,ll>>> M;
     for(ll i=0;i<lines.size();i++) {                
         for(ll j=i+1;j<lines.size();j++) {            
             if(true) {
-                if(intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second)!=invalid){
-                    M[intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second)]+=2;
+                if(intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second)!=invalid and s.find(intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second))==s.end()){
+                    M[intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second)].insert(lines[i].second);
+                    M[intersect(lines[i].first,lines[j].first,lines[i].second,lines[j].second)].insert(lines[j].second);
                 }
             }            
         }
     }
-    
+    ll third=0;
     for(ll i=0;i<n;i++) {
-        for(ll j=i+1;j<n;j++) {
-            if(arr[j].second-arr[i].second==(-arr[j].first+arr[i].first)) {
-                M[arr[i]]+=2;
-                M[arr[j]]+=2;
+        ll temp=2;
+        for(ll j=0;j<n;j++) {
+            if(i!=j) {
+                if(online(arr[i],arr[j])) {
+                    temp++;
+                }
+            }
+        }
+        third=max(third,temp);
+    }
+    for(auto x:M) {
+        ans2=min(ans2,(ll)(total-(x.second.size())));
+    }
+    
+    map<pair<ll,ll>,set<pair<ll,ll>>> slope1;
+    map<pair<ll,ll>,set<pair<ll,ll>>> slopeneg1;
+    for(ll i=0;i<n;i++) {
+        pair<ll,ll> tempp={1,arr[i].second-arr[i].first};
+        for(ll j=0;j<n;j++) {
+            if(linept(tempp,arr[j])) {
+                slope1[tempp].insert(arr[j]);
             }
         }
     }
-    for(auto &x:M) {
-        cout<<x.first.first<<" "<<x.first.second<<" -> "<<x.second<<endl;
-        if(s.find(x.first)!=s.end()) {
-            x.second++;
+    for(ll i=0;i<n;i++) {
+        pair<ll,ll> tempp={-1,arr[i].second+arr[i].first};
+        for(ll j=0;j<n;j++) {
+            if(linept(tempp,arr[j])) {
+                slopeneg1[tempp].insert(arr[j]);
+            }
         }
     }
-    for(auto x:M) {
-        ans2=min(ans2,total-x.second);
+    ll anss=0;
+    for(auto x:slope1) {
+        anss=max(anss,(ll)x.second.size());
     }
-    cout<<min(ans,ans2)<<endl;
+    ll ansc=0;
+    for(auto x:slopeneg1) {
+        ansc=max(ansc,(ll)x.second.size());
+    }
+    ll ans3=(2*n)-anss-ansc;
+
+
+    map<pair<ll,ll>,set<pair<ll,ll>>> slope0;
+    map<pair<ll,ll>,set<pair<ll,ll>>> slopeinf;
+    for(ll i=0;i<n;i++) {
+        pair<ll,ll> tempp={0,arr[i].second};
+        for(ll j=0;j<n;j++) {
+            if(linept(tempp,arr[j])) {
+                slope0[tempp].insert(arr[j]);
+            }
+        }
+    }
+    for(ll i=0;i<n;i++) {
+        pair<ll,ll> tempp={inf,-arr[i].first};
+        for(ll j=0;j<n;j++) {
+            if(linept(tempp,arr[j])) {
+                slopeinf[tempp].insert(arr[j]);
+            }
+        }
+    }
+    ll ans11=0;
+    ll ans12=0;
+    for(auto x:slope0) ans11=max(ans11,(ll)x.second.size());
+    for(auto x:slopeinf) ans12=max(ans12,(ll)x.second.size());
+    ll ans4=(2*n)-ans11-ans12;
+    cout<<min({ans3,ans4,ans2,((2*n)-third)})<<endl;
 }
 
 int32_t main() {
