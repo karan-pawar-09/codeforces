89720689cfd830788e0a79a09f0340decb018323
Karan <karanbpawar@gmail.com>
2021-09-11 11:31:05 +0530

    k

--- shubhampp.cpp
+++ shubhampp.cpp
@@ -8,44 +8,44 @@ using namespace std;
 #define all(ar) ar.begin(),ar.end()
 #define endl '\n'
 
-bool comp(pair<int,pair<int,int>> a,pair<int,pair<int,int>> b) {
+bool comp(pair<ll,pair<ll,ll>> a,pair<ll,pair<ll,ll>> b) {
     return((a.second.first*b.second.second)>(b.second.first*a.second.second));
 }
 void solve() {
-    int n;
+    ll n;
     cin>>n;
-    vector<int> cost(n),nutrients(n),min_quantity(n),max_quantity(n);
-    int m;
-    for(int i=0;i<n;i++) cin>>cost[i];
+    vector<ll> cost(n),nutrients(n),min_quantity(n),max_quantity(n);
+    ll m;
+    for(ll i=0;i<n;i++) cin>>cost[i];
     cin>>n;
-    for(int i=0;i<n;i++) cin>>nutrients[i];
+    for(ll i=0;i<n;i++) cin>>nutrients[i];
     cin>>n;
-    for(int i=0;i<n;i++) cin>>min_quantity[i];
+    for(ll i=0;i<n;i++) cin>>min_quantity[i];
     cin>>n;
-    for(int i=0;i<n;i++) cin>>max_quantity[i];
+    for(ll i=0;i<n;i++) cin>>max_quantity[i];
     cin>>m;
-    int money=0;
-    int ans=0;
-    for(int i=0;i<n;i++) {
+    ll money=0;
+    ll ans=0;
+    for(ll i=0;i<n;i++) {
         money+=min_quantity[i]*cost[i];
         ans+=nutrients[i]*min_quantity[i];
         max_quantity[i]-=min_quantity[i];
     }
-    vector<pair<int,pair<int,int>>> arr(n);
-    for(int i=0;i<n;i++) {
+    vector<pair<ll,pair<ll,ll>>> arr(n);
+    for(ll i=0;i<n;i++) {
         arr[i].first=max_quantity[i];
         arr[i].second.first=nutrients[i];
         arr[i].second.second=cost[i];
     }
     sort(all(arr),comp);
-    int mon=m-money;
-    for(int i=0;i<n && mon>0;i++) {
-        int temp=mon/arr[i].second.second;
+    ll mon=m-money;
+    for(ll i=0;i<n && mon>0;i++) {
+        ll temp=mon/arr[i].second.second;
         temp=min(temp,arr[i].first);
         mon-=temp*arr[i].second.second;
         ans+=temp*arr[i].second.first;
     }
-    if(mon<0) {
+    if(money>m) {
         cout<<0<<endl;
     }
     else
