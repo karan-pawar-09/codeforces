c90f62c136a7d94a0bfafb042e948d405fc40aea
Karan <karanbpawar@gmail.com>
2021-10-31 12:06:08 +0530

    k

---
+++ tejasss1.cpp
@@ -0,0 +1,34 @@
+/*
+    author:Karan
+    created:26.10.2021 22:02:07
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(ar) ar.begin(),ar.end()
+#define endl '\n'
+
+bool solve() {
+    int n,x;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    int t=1;
+    // cin>>t;
+    while(t--) {
+        cout<<solve();
+    }
+}
+
+
+
